e3271bb818c38eaa9b5d3c9da06dff9480577cb6
Kait Kasak <kait@snowhound.eu>
2020-03-05 19:11:58 +0200

    Initial demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import reactifyWc from 'reactify-wc';
 import './App.css';
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-grid';
+import '@vaadin/vaadin-text-field';
+
+const VaadinButton = reactifyWc("vaadin-button");
+const VaadinGrid = reactifyWc("vaadin-grid");
+const VaadinGridColumn = reactifyWc("vaadin-grid-column");
+const VaadinTextField = reactifyWc("vaadin-text-field");
+
+
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (e) => setValue(e.target.value);
+  return [value, onChange];
+};
 
 function App() {
+  const [firstName, setFirstName] = useInput('');
+  const [lastName, setLastName] = useInput('');
+  const [people, setPeople] = useState([]);
+
+  const onClick = () => setPeople(people.concat([{firstName, lastName}]));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form">
+        <VaadinTextField label="First Name" value={firstName} onInput={setFirstName} />
+        <VaadinTextField label="Last Name" value={lastName} onInput={setLastName} />
+        <VaadinButton onClick={onClick}> Add </VaadinButton>
+      </div>
+      <VaadinGrid items={people}>
+        <VaadinGridColumn path="firstName" header="First name" /> 
+        <VaadinGridColumn path="lastName" header="Last name" />
+      </VaadinGrid>
     </div>
   );
 }
